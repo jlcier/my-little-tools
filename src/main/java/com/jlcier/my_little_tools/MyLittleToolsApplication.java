c25d7927ce2b3685867cdd57abb0b6560bead78e
jlcier <145858443+jlcier@users.noreply.github.com>
2025-02-20 00:44:52 -0300

    start project

---
+++ src/main/java/com/jlcier/my_little_tools/MyLittleToolsApplication.java
@@ -0,0 +1,13 @@
+package com.jlcier.my_little_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLittleToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLittleToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlcier/my_little_tools/MyLittleToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlcier.my_little_tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLittleToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
